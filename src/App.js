16020a075cb3bb08bd8c8860a563a05d5755228c
henry-shih4 <100496651+henry-shih4@users.noreply.github.com>
2022-12-30 16:54:44 -0600

    menu page layout and spacing
    added footer and contact to main app page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
+import Map from "./components/Map";
+import Footer from "./components/Footer";
 import Home from "./pages/Home";
 import Menu from "./pages/Menu";
 
@@ -9,14 +11,21 @@ function App() {
   window.onbeforeunload = function () {
     window.scrollTo(0, 0);
   };
+
   return (
     <div className="App">
       <BrowserRouter>
         <Header />
         <Routes>
-          <Route path="/" element={<Home />}></Route>
+          <Route
+            onUpdate={() => window.scrollTo(0, 0)}
+            path="/"
+            element={<Home />}
+          ></Route>
           <Route path="menu" element={<Menu />}></Route>
         </Routes>
+        <Map />
+        <Footer />
       </BrowserRouter>
     </div>
   );
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -14,8 +14,6 @@ export default function Home() {
       <Features />
       <Reviews />
       <Gallery />
-      <Map />
-      <Footer />
     </>
   );
 }
